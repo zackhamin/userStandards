5e77e1fa69a0b8b136cc0827fd10dc7f41c41c99
Zack <ishaqamincoop@icloud.com>
2020-04-01 11:30:01 +0100

    first commit

---
+++ userDefaults/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  userDefaults
+//
+//  Created by Ishaq Amin on 01/04/2020.
+//  Copyright © 2020 Ishaq Amin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let defaults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
